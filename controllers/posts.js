341b6e21e4159d4f646470312dc4d8034ec31a2d
Anaiahm <anaiahmaddox@gmail.com>
2024-08-28 02:53:40 -0400

    added posts controller, CRUD routes, and comments

--- controllers/posts.js
+++ controllers/posts.js
@@ -37,5 +37,80 @@ router.get('/:postId', async (req, res) => {
         res.status(500).json(error);
       }
 });
+router.put('/:postId', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+    
+        if (!post.author.equals(req.user._id)) {
+          return res.status(403).send("You're not allowed to do that!");
+        }
+    
+        const updatedPost = await Post.findByIdAndUpdate(
+          req.params.postId,
+          req.body,
+          { new: true }
+        );
+    
+        updatedPost._doc.author = req.user;
+    
+        res.status(200).json(updatedPost);
+      } catch (error) {
+        res.status(500).json(error);
+      }
+});
+router.delete('/:postId', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+    
+        if (!post.author.equals(req.user._id)) {
+          return res.status(403).send("You're not allowed to do that!");
+        }
+    
+        const deletedPost = await Post.findByIdAndDelete(req.params.postId);
+        res.status(200).json(deletedPost);
+      } catch (error) {
+        res.status(500).json(error);
+      }
+});
+// controllers/hoots.js
+
+router.post('/:postId/comments', async (req, res) => {
+    try {
+        req.body.author = req.user._id;
+        const post = await Post.findById(req.params.postId);
+        post.comments.push(req.body);
+        await post.save();
+    
+        const newComment = post.comments[post.comments.length - 1];
+    
+        newComment._doc.author = req.user;
+    
+        res.status(201).json(newComment);
+      } catch (error) {
+        res.status(500).json(error);
+      }
+});
+router.put('/:postId/comments/:commentId', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        const comment = post.comments.id(req.params.commentId);
+        comment.text = req.body.text;
+        await post.save();
+        res.status(200).json({ message: 'Ok' });
+      } catch (err) {
+        res.status(500).json(err);
+      }
+});
+router.delete('/:postId/comments/:commentId', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        post.comments.remove({ _id: req.params.commentId });
+        await post.save();
+        res.status(200).json({ message: 'Ok' });
+      } catch (err) {
+        res.status(500).json(err);
+      }
+});
+
 
 module.exports = router;
--- models/post.js
+++ models/post.js
@@ -1,38 +1,38 @@
 const mongoose = require('mongoose');
-const Post = mongoose.model('Post', postSchema);
 
+const commentSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true
+    },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+  },
+  { timestamps: true }
+);
 const postSchema = new mongoose.Schema(
     {
-      title: {
-        type: String,
-        required: true,
-      },
-      text: {
-        type: String,
-        required: true,
-      },
-      category: {
-        type: String,
-        required: true,
-        enum: ['Cheats', 'Tips', 'Build Challenges', 'GamePlay Challenges'],
-      },
-      author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-      comments: [commentSchema],
+        title: {
+            type: String,
+            required: true,
+        },
+        text: {
+            type: String,
+            required: true,
+        },
+        category: {
+            type: String,
+            required: true,
+            enum: ['Cheats', 'Tips', 'Build Challenges', 'GamePlay Challenges'],
+        },
+        author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        comments: [commentSchema],
     },
     { timestamps: true }
-  );
+);
+
+const Post = mongoose.model('Post', postSchema);
 
 
-
-  const commentSchema = new mongoose.Schema(
-    {
-      text: {
-        type: String,
-        required: true
-      },
-      author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-    },
-    { timestamps: true }
-  );
 
   module.exports = Post;
